f4831f412a0e6078e4b8114a2c79dfc827082be5
johnfelix2911 <156358964+johnfelix2911@users.noreply.github.com>
2024-02-16 02:11:30 -0800

    Update AstarVisualisation.py

--- AstarVisualisation.py
+++ AstarVisualisation.py
@@ -39,43 +39,14 @@ print(graph)
 vis=[]
 q=[]
 
-#applying bfs
-'''def bfs(node):
-    q.append(node)
-    vis.append(node)
-    img[node[1]][node[0]][0] = 0
-    img[node[1]][node[0]][1] = 0
-    img[node[1]][node[0]][2] = 255
-    while True:
-        m=q.pop(0)
-        for n in graph[m]:
-            if n not in vis:
-                vis.append(n)
-                img[n[1]][n[0]][0] = 0
-                img[n[1]][n[0]][1] = 0
-                img[n[1]][n[0]][2] = 255
-                cv2.imshow("hehe", img)
-                cv2.waitKey(100)
-                q.append(n)
-cv2.destroyAllWindows()
-
 def minimum(lis):
     min = lis[0]
     for i in lis:
         if i<min:
             min = i
-    return min'''
+    return min
 
-def minPos(lis):
-    min = lis[0]
-    pos = 0
-    for i in range(len(lis)):
-        if lis[i]<min:
-            pos = i
-    return pos
-
-
-def rrtStar(node,goal):
+def AStar(node,goal):
     vis.append(node)
     img[node[1]][node[0]][0] = 0
     img[node[1]][node[0]][1] = 0
@@ -92,11 +63,11 @@ def rrtStar(node,goal):
     next_node_pos = minPos(l_dist)
     next_node = l_point[next_node_pos]
     if next_node!=goal:
-        rrtStar(next_node,goal)
+        AStar(next_node,goal)
     else:
         cv2.ellipse(img, goal, (10,10), 0, 0, 360, (0,0,255), 2)
         cv2.imshow("hehe", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-rrtStar((296,293), (498,490))
+AStar((296,293), (498,490))
